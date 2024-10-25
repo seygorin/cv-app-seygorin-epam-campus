b05ef9e87e900fc6c978586fc121ef205362b8ab
Sergey <seygorin@gmail.com>
2024-10-25 11:30:51 +0500

    feat: added loading component to inner page

--- src/pages/inner/index.tsx
+++ src/pages/inner/index.tsx
@@ -2,8 +2,10 @@ import {useTranslation} from 'next-i18next'
 import {useRouter} from 'next/router'
 import Link from 'next/link'
 import Image from 'next/image'
+import {useState} from 'react'
 import PhotoBox from '@/components/PhotoBox'
 import Button from '@/components/Button'
+import Loading from '@/components/Loading'
 
 import styles from './index.module.css'
 
@@ -11,35 +13,52 @@ export default function Inner() {
   const {t} = useTranslation('common')
   const router = useRouter()
 
+  const [imageLoading, setImageLoading] = useState(true)
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.buttonWow}>
-        <Link
-          target='_blank'
-          rel='noopener noreferrer'
-          href='https://science.nasa.gov/image-detail/37893980612-307324af83-o/'
-        >
-          {t('wow')}
-        </Link>
+    <>
+      {imageLoading && (
+        <div className={styles.loadingContainer}>
+          <Loading />
+        </div>
+      )}
+      <div
+        className={styles.wrapper}
+        style={{display: imageLoading ? 'none' : 'block'}}
+      >
+        <div className={styles.buttonWow}>
+          <Link
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://science.nasa.gov/image-detail/37893980612-307324af83-o/'
+          >
+            {t('wow')}
+          </Link>
+        </div>
+        <div className={styles.backgroundImage}>
+          <Image
+            src='/assets/background.jpg'
+            alt={t('background')}
+            fill
+            sizes='100vw'
+            style={{objectFit: 'cover'}}
+            quality={75}
+            priority
+            onLoad={() => {
+              setImageLoading(false)
+            }}
+          />
+        </div>
+        <div className={styles.container}>
+          <PhotoBox
+            name={t('name')}
+            title={t('programmerCreativeThinker')}
+            description={t('problemSolvingDescription')}
+            avatar='/assets/avatar.jpg'
+          />
+          <Button text={t('knowMore')} onClick={() => router.push('/home')} />
+        </div>
       </div>
-      <div className={styles.backgroundImage}>
-        <Image
-          src='/assets/background.jpg'
-          alt={t('background')}
-          fill
-          style={{objectFit: 'cover'}}
-          quality={75}
-        />
-      </div>
-      <div className={styles.container}>
-        <PhotoBox
-          name={t('name')}
-          title={t('programmerCreativeThinker')}
-          description={t('problemSolvingDescription')}
-          avatar='/assets/avatar.jpg'
-        />
-        <Button text={t('knowMore')} onClick={() => router.push('/home')} />
-      </div>
-    </div>
+    </>
   )
 }
